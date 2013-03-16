87db02f441030b2ae5ad6f127ca70f1eadfc4cc2
robrobbins <rob.robb.ns@gmail.com>
2013-03-16 14:49:58 -0700

    updates for change filter

--- build/debug/sudo-x.js
+++ build/debug/sudo-x.js
@@ -1459,23 +1459,40 @@ sudo.delegates.Change = function(data) {
 };
 // Delegates inherit from Model
 sudo.delegates.Change.prototype = Object.create(sudo.Model.prototype);
+// ###addFilter
+// Place an entry into this object's hash of filters
+//
+// `param` {string} `key`
+// `param` {string} `val`
+// `returns` {object} this
+sudo.delegates.Change.prototype.addFilter = function addFilter(key, val) {
+	this.get('filters')[key] = val;
+	return this;
+},
+// ###filter
 // Change records are delivered here and filtered, calling any matching
 // methods specified in `this.get('filters').
 //
 // `returns` {Object} a call to the specified _delegator_ method, passing
 // a hash containing:
 // 1. the `type` of Change
-// 2. the value located at the key/path
-// 3. the `oldValue` of the key if present
+// 2. the `name` of the changed property
+// 3. the value located at the key/path
+// 4. the `oldValue` of the key if present
 sudo.delegates.Change.prototype.filter = function filter(change) {
-	var filters = this.data.filters, name = change.name, obj = {};
+	var filters = this.data.filters, name = change.name,
+		type = change.type, obj = {};
 	// does my delegator care about this?
 	if(name in filters && filters.hasOwnProperty(name)) {
 		// assemble the object to return to the method
-		obj.type = change.type;
-		obj.value = name.indexOf('.') === -1 ? change.object[change.name] :
-			sudo.getPath(name, change.object);
+		obj.type = type;
+		obj.name = name;
 		obj.oldValue = change.oldValue;
+		// delete operations will not have any value so no need to look
+		if(type !== 'deleted') {
+			obj.value = name.indexOf('.') === -1 ? change.object[change.name] :
+				sudo.getPath(name, change.object);
+		}
 		return this.delegator[filters[name]].call(this.delegator, obj);
 	}
 };
